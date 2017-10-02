c4ee2e5a9e643cdd8149d3951fc1fc5beb34d2f4
Claidson <>
2017-10-02 20:59:32 -0300

    first

---
+++ src/main/java/com/mycompany/chatsimples/Programa.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.chatsimples;
+
+/**
+ *
+ * @author user
+ */
+import javax.swing.JOptionPane;
+
+public class Programa {
+
+    public static void main(String[] args) {
+        String ip = (String) JOptionPane.showInputDialog("Informe o IP", "192.168.0.");
+        int porta = Integer.parseInt(JOptionPane.showInputDialog("Informe a Porta", "5000"));
+
+        Conexao c = new Conexao(ip, porta);
+
+        JanelaChat j = new JanelaChat(c);
+        j.setLocationRelativeTo(null);
+        j.setVisible(true);
+    }
+}
